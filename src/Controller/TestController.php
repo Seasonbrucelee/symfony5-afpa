035b9c31a464bed4f979e18b09cdfcaab9c37cc8
Seasonbrucelee <sezen39@hotmail.com>
2022-04-11 14:01:13 +0200

    index et test.html et controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,19 +11,26 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
+        
         return $this->render('test/index.html.twig', [
             'controller_name' => 'Mon TestController',
         ]);
+        
+        /*return $this->json(['username'=> 'jane.doe']);*/
+        
     }
 
     /**
     * @Route("/test/{id}", name="test_id", methods={"GET"}, requirements={"id"="\d+"})
     */
+
     #[Route('/test/{id}', name:'test_id', methods:"GET", requirements: ['id' => '\d+'])]
     public function test_id($id): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'Mon TestController ❤',
+        //dd($id); //* permet de debuger */
+        return $this->render('test/test.html.twig',[
+            'controller_name' => 'Mon TestController avec Id ❤',
+            'id' =>$id,
         ]);
     }
 }
